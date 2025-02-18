6f8f224908776b52590082115de85c61ee9bcfc7
Matteus Santos <matteus.py@gmail.com>
2025-02-18 10:56:10 -0300

    add user entity and users service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/user.entity';
 
 @Module({
   imports: [
@@ -13,7 +14,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       username: 'root',
       password: 'root',
       database: 'tickets',
-      entities: [],
+      entities: [User],
       synchronize: true,
     }),
     UsersModule,
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,23 @@
+import { Entity, Column, PrimaryGeneratedColumn, Unique } from 'typeorm';
+
+@Entity()
+@Unique(['email'])
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  updatedAt: Date;
+
+  @Column()
+  createdAt: Date;
+}
