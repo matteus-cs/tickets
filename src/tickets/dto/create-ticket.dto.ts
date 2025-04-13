06d70eb636deb9101dda28e26ee742a9a11749f7
Matteus Santos <matteus.py@gmail.com>
2025-04-13 16:21:23 -0300

    documenta endpoint tickets

--- src/tickets/dto/create-ticket.dto.ts
+++ src/tickets/dto/create-ticket.dto.ts
@@ -1,15 +1,25 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class CreateTicketDto {
+  @ApiProperty({
+    type: 'string',
+  })
   @IsString()
   @IsNotEmpty()
   location: string;
 
+  @ApiProperty({
+    type: 'string',
+  })
   @IsNumber()
   @IsNotEmpty()
   price: number;
 
+  @ApiProperty({
+    type: 'number',
+    required: false,
+  })
   @IsString()
   @IsOptional()
   quantity?: number;
--- src/tickets/tickets.controller.ts
+++ src/tickets/tickets.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Post, Body, Request, UseGuards } from '@nestjs/common';
 import { TicketsService } from './tickets.service';
 import { CreateTicketDto } from './dto/create-ticket.dto';
 import { AuthGuard } from '@/auth/auth.guard';
+import { ApiBody, ApiResponse, ApiUnauthorizedResponse } from '@nestjs/swagger';
 
 @Controller('events/:eventId/tickets')
 export class TicketsController {
@@ -9,6 +10,9 @@ export class TicketsController {
 
   @UseGuards(AuthGuard)
   @Post()
+  @ApiBody({ type: [CreateTicketDto] })
+  @ApiResponse({ status: 201, description: 'Successfully created' })
+  @ApiUnauthorizedResponse({ description: 'When a user is not a partner' })
   create(@Body() createTicketDto: CreateTicketDto[], @Request() req) {
     return this.ticketsService.create(
       createTicketDto,
