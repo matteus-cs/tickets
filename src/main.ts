d1732251e14d121bde88054a7b2c547bb1eab292
Matteus Santos <matteus.py@gmail.com>
2025-05-05 21:55:16 -0300

    add helmet

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 import { json, raw } from 'body-parser';
 import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: '*' });
+  app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
 
   app.use(
